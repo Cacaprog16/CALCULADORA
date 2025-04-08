bce88269c23cc884a4251a25b1c92c46a3df0357
Cacaprog16 <136651641+Cacaprog16@users.noreply.github.com>
2025-04-08 15:52:41 +0000

    Add files via upload

---
+++ script_para_calculadora.sh
@@ -0,0 +1,38 @@
+#
+
+
+echo "Para a execução do programa será necessario a istalação da linguagem Python."
+
+echo "Neste momento irei reconheceer se você tem a linguagem em seu sistema."
+
+echo "Caso necessario insira sua senha:"
+
+echo "Aguarde, verificando............"
+
+sudo apt update
+
+echo "##############################################################################################################################"
+
+echo "Pronto!!!"
+
+echo "O processo de localização dos pacotes foi finalizado."
+
+echo "Agora irei instalar ou atualizar o seu Python."
+
+sudo apt install python3
+
+echo "###############################################################################################################################"
+
+echo "Pronto processo finalizado, você esta com o Python em seu sistema intalado ou atualizado."
+
+echo "Agora você pode executar a aplicação da Calculadora!."
+
+echo "Para isto faça os seguinte."
+
+echo "Primeiro faça o download do programa com o seguinte nome:<Calculadora.py>."
+
+echo "Após isto abra o seu terminal localize o arquivo baixado,  execute com   ./<Nome do Arquivo> no nosso caso Calculadora.py. "
+
+echo "Caso goste, compartilhe ou faça melhorias no projetos."
+
+
